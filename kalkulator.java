1df609ad6bdce386b73e207d990a6a0e4b7aec42
dianpetrusp-creator <dianpetrusp@gmail.com>
2025-10-17 19:10:57 +0700

    pair5

--- kalkulator.java
+++ kalkulator.java
@@ -4,7 +4,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class kalkulatoruts {
+public class kalkulator {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
