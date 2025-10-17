ca27a1dd0243a6d0f0c2cd70c63e01675333905d
dianpetrusp-creator <dianpetrusp@gmail.com>
2025-10-17 16:44:55 +0700

    PAIR 05
    Telah Selesai UTS

---
+++ fibonacci_sum.java
@@ -0,0 +1,24 @@
+//12S25020 - Julia Simatupang
+//1225057 - Dian Petrus Panjaitan
+import java.util.*;
+import java.lang.Math;
+
+public class fibonacci {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, b, fib, jumlah, n, i;
+
+        n = Integer.parseInt(input.nextLine());
+        a = 1;
+        b = 1;
+        jumlah = 0;
+        for (i = 1; i <= n; i++) {
+            jumlah = jumlah + a;
+            fib = a;
+            a = b;
+            b = fib + b;
+        }
+        System.out.println(jumlah);
+    }
+}
---
+++ kalkulator.java
@@ -0,0 +1,43 @@
+//12S25020 - Julia Simatupang
+//12S25057 - Dian Petrus Panjaitan
+import java.util.*;
+import java.lang.Math;
+
+public class kalkulator {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int penjumlahan, pengurangan, perkalian, pembagian;
+        String pilihan;
+        int bilangan1, bilangan2;
+
+        pilihan = input.nextLine();
+        if (pilihan.equals("1")) {
+            bilangan1 = Integer.parseInt(input.nextLine());
+            bilangan2 = Integer.parseInt(input.nextLine());
+            penjumlahan = bilangan1 + bilangan2;
+            System.out.println(penjumlahan);
+        } else {
+            if (pilihan.equals("2")) {
+                bilangan1 = Integer.parseInt(input.nextLine());
+                bilangan2 = Integer.parseInt(input.nextLine());
+                pengurangan = bilangan1 - bilangan2;
+                System.out.println(pengurangan);
+            } else {
+                if (pilihan.equals("3")) {
+                    bilangan1 = Integer.parseInt(input.nextLine());
+                    bilangan2 = Integer.parseInt(input.nextLine());
+                    perkalian = bilangan1 * bilangan2;
+                    System.out.println(perkalian);
+                } else {
+                    if (pilihan.equals("4")) {
+                        bilangan1 = Integer.parseInt(input.nextLine());
+                        bilangan2 = Integer.parseInt(input.nextLine());
+                        pembagian = (double) bilangan1 / bilangan2;
+                        System.out.println(pembagian);
+                    }
+                }
+            }
+        }
+    }
+}
